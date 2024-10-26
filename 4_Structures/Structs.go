a0b65891174c08eac0d2cda0d546088f55603afb
Rajdeep Mandal <mrajdeep651@gmail.com>
2024-10-26 23:51:26 +0530

    Type Assertions

--- 4_Structures/Structs.go
+++ 4_Structures/Structs.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	// "log"
 	"math"
 )
 
@@ -141,6 +142,29 @@ func (e email) format() string {
 
 //---------------------------------------------------------------------
 
+//Type Assertions in GO
+
+/* -> we want to check if s is a circle in order to cast it into its underlying concrete type
+   -> we know s is an instance of the shape interface, but we do not know if it's also a circle
+   -> c is a new circle struct cast from s
+   -> ok is true if s is indeed a circle, or false if s is NOT a circle*/
+
+// type shape interface {
+// 	area() float64
+// }
+
+// type circle struct {
+// 	radius float64
+// }
+
+// c, ok := s.(circle)
+// if !ok {
+// 	// log an error if s isn't a circle
+// 	log.Fatal("s is not a circle")
+// }
+
+// radius := c.radius
+
 func main() {
 	// myCar := car{}
 	// myCar.brand = "BMW"
@@ -176,14 +200,15 @@ func main() {
 	// fmt.Println(r.area()) //50
 
 	//Interfaces
-	r := rectan{
-		width:  5,
-		height: 10,
-	}
-	fmt.Println(r.area(), r.perimeter())
+	// r := rectan{
+	// 	width:  5,
+	// 	height: 10,
+	// }
+	// fmt.Println(r.area(), r.perimeter())
 
-	c := circle{
-		radius: 20,
-	}
-	fmt.Println(c.area(), c.perimeter())
+	// c := circle{
+	// 	radius: 20,
+	// }
+	// fmt.Println(c.area(), c.perimeter())
+
 }
