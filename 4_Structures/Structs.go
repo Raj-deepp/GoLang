02c6fb83a1f6dc586e25c5776f4bd44eff2b9345
Rajdeep Mandal <mrajdeep651@gmail.com>
2024-10-26 17:53:12 +0530

    Struct Methods

--- 4_Structures/Structs.go
+++ 4_Structures/Structs.go
@@ -1,4 +1,5 @@
 package main
+
 import "fmt"
 
 type car struct {
@@ -10,10 +11,11 @@ type car struct {
 	backWheel  wheel
 }
 
-type wheel struct{
-	radius int
+type wheel struct {
+	radius   int
 	material string
 }
+
 //---------------------------------------------------------------
 
 type messageToSend struct {
@@ -36,51 +38,82 @@ func canSendMessage(mToSend messageToSend) bool {
 	}
 	return true
 }
+
 //--------------------------------------------------------------
 
 //Embedded Structs
 type car2 struct {
-  make string
-  model string
+	make  string
+	model string
 }
 
 type truck struct {
-  // "car" is embedded, so the definition of a
-  // "truck" now also additionally contains all
-  // of the fields of the car struct
-  car2
-  bedSize int
+	// "car" is embedded, so the definition of a
+	// "truck" now also additionally contains all
+	// of the fields of the car struct
+	car2
+	bedSize int
 }
+
 //------------------------------------------------------------------
 
+//Struct methods in Go
+type rectangle struct {
+	width  int
+	height int
+}
 
-func main(){
+// area has a receiver of (r rect)
+// rect is the struct
+// r is the placeholder
+func (r rectangle) area() int {
+	return r.width * r.height
+}
+
+
+type authenticationInfo struct {
+	username string
+	password string
+}
+func (authI authenticationInfo) getBasicAuth() string{
+	return fmt.Sprintf("Authorization: Basic %s: %s", authI.username, authI.password)
+}
+
+//-------------------------------------------------------------------------
+
+func main() {
 	// myCar := car{}
 	// myCar.brand = "BMW"
 	// myCar.frontWheel.radius = 5
 	// fmt.Println(myCar)
 
+	// sender := user{name: "Alice", number: 12345}
+	// recipient := user{name: "Bob", number: 67890}
 
-	sender := user{name: "Alice", number: 12345}
-	recipient := user{name: "Bob", number: 67890}
+	// msg := messageToSend{
+	// 	message:   "Hello!",
+	// 	sender:    sender,
+	// 	recipient: recipient,
+	// }
 
-	msg := messageToSend{
-		message:   "Hello!",
-		sender:    sender,
-		recipient: recipient,
+	// canSend := canSendMessage(msg)
+	// fmt.Println("Can send message:", canSend)
+
+	// myCar2 := car2{}
+	// myTruck := truck{}
+	// myCar2.make = "TATA Motors"
+	// myCar2.model = "PUNCH"
+	// myTruck.make= "TATA Motors"
+
+	// fmt.Println("Car:", myCar2)
+	// fmt.Println("Truck:", myTruck)
+
+
+	//Struct Methods
+	r := rectangle{
+		width:  5,
+		height: 10,
 	}
-
-	canSend := canSendMessage(msg)
-	fmt.Println("Can send message:", canSend)
-
-
-	myCar2 := car2{}
-	myTruck := truck{}
-	myCar2.make = "TATA Motors"
-	myCar2.model = "PUNCH"
-	myTruck.make= "TATA Motors"
-
-	fmt.Println("Car:", myCar2)
-	fmt.Println("Truck:", myTruck)
+	fmt.Println(r.area()) //50
 
 }
