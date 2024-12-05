8bc5cd2ea052c70a0fcf92d63871c635c8568372
Rajdeep Mandal <mrajdeep651@gmail.com>
2024-12-06 00:31:30 +0530

    mystring Package to Reverse a String

---
+++ HELLOGO/go.mod
@@ -0,0 +1,7 @@
+module github.com/Raj-deepp/reverse
+
+go 1.23.2
+
+require github.com/Raj-deepp/mystrings v0.0.0
+
+replace github.com/Raj-deepp/mystrings => ../11_Packages
---
+++ HELLOGO/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Raj-deepp/mystrings"
+)
+
+func main() {
+	fmt.Println(
+		mystrings.Reverse("This is a reversed string from mystring package"),
+	)
+}
