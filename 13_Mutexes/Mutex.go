60f82092a5b339126b11948eb1ff3154e96056a0
Rajdeep Mandal <mrajdeep651@gmail.com>
2024-12-12 19:55:20 +0530

    RW Mutex

--- 13_Mutexes/Mutex.go
+++ 13_Mutexes/Mutex.go
@@ -39,15 +39,76 @@ func (sc safeCounter) slowVal(key string) int {
 	return sc.counts[key]
 }
 
+//-------------------------------------------------------------------------------------------------
+
+//RW MUTEX
+// The sync.RWMutex also has these methods for concurrent reads:
+// RLock()
+// RUnlock()
+
+type safeCounter2 struct {
+	counts map[string]int
+	mux    *sync.RWMutex
+}
+
+func (sc *safeCounter2) inc2(key string) {
+	sc.mux.Lock()
+	defer sc.mux.Unlock()
+	sc.slowIncrement2(key)
+}
+
+func (sc *safeCounter2) val2(key string) int {
+	sc.mux.RLock()
+	defer sc.mux.RUnlock()
+	return sc.counts[key]
+}
+
+func (sc *safeCounter2) slowIncrement2(key string) {
+	tempCounter := sc.counts[key]
+	time.Sleep(time.Microsecond)
+	tempCounter++
+	sc.counts[key] = tempCounter
+}
+
+//-------------------------------------------------------------------------------------------------
+
 func main() {
-	sc := &safeCounter{
+	// sc := &safeCounter{
+	// 	counts: make(map[string]int),
+	// 	mux:    &sync.Mutex{},
+	// }
+
+	// // Create a WaitGroup to wait for goroutines to finish
+	// var wg sync.WaitGroup
+	// keys := []string{"a", "b", "c"}
+
+	// // Increment counters concurrently
+	// for _, key := range keys {
+	// 	wg.Add(1)
+	// 	go func(key string) {
+	// 		defer wg.Done()
+	// 		for i := 0; i < 1000; i++ {
+	// 			sc.inc(key)
+	// 		}
+	// 	}(key)
+	// }
+
+	// wg.Wait()
+
+	// // Retrieve and print final counter values
+	// for _, key := range keys {
+	// 	fmt.Printf("Key: %s, Value: %d\n", key, sc.val(key))
+	// }
+
+	//Rw MUTEX
+	sc := &safeCounter2{
 		counts: make(map[string]int),
-		mux:    &sync.Mutex{},
+		mux:    &sync.RWMutex{},
 	}
 
 	// Create a WaitGroup to wait for goroutines to finish
 	var wg sync.WaitGroup
-	keys := []string{"a", "b", "c"}
+	keys := []string{"x", "y", "z"}
 
 	// Increment counters concurrently
 	for _, key := range keys {
@@ -55,7 +116,7 @@ func main() {
 		go func(key string) {
 			defer wg.Done()
 			for i := 0; i < 1000; i++ {
-				sc.inc(key)
+				sc.inc2(key)
 			}
 		}(key)
 	}
@@ -64,6 +125,6 @@ func main() {
 
 	// Retrieve and print final counter values
 	for _, key := range keys {
-		fmt.Printf("Key: %s, Value: %d\n", key, sc.val(key))
+		fmt.Printf("Key: %s, Value: %d\n", key, sc.val2(key))
 	}
 }
