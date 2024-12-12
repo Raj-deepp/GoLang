faad278b7d324d1d61c04311604b3903cf38cee8
Rajdeep Mandal <mrajdeep651@gmail.com>
2024-12-13 00:55:24 +0530

    Parametric Constraints

--- 14_Generics/Generic.go
+++ 14_Generics/Generic.go
@@ -1,6 +1,9 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // Put simply, generics allow us to use variables to refer to specific types. This is an amazing feature because it allows us to write abstract functions that drastically reduce code duplication.
 
@@ -19,7 +22,7 @@ func getLast[T any](s []T) T {
 	return s[len(s)-1]
 }
 
-//CONSTRAINTS
+// CONSTRAINTS
 type lineItem interface {
 	GetCost() float64
 	GetName() string
@@ -33,6 +36,82 @@ func chargeForLineItem[T lineItem](newItem T, oldItems []T, balance float64) ([]
 	oldItems = append(oldItems, newItem)
 	return oldItems, newBalance, nil
 }
+
+//-------------------------------------------------------------------------------------------------
+
+// Parametric Constraints
+type biller[C customer] interface {
+	Charge(C) bill
+	Name() string
+}
+
+// don't edit below this line
+
+type userBiller struct {
+	Plan string
+}
+
+func (ub userBiller) Charge(u user) bill {
+	amount := 50.0
+	if ub.Plan == "pro" {
+		amount = 100.0
+	}
+	return bill{
+		Customer: u,
+		Amount:   amount,
+	}
+}
+
+func (sb userBiller) Name() string {
+	return fmt.Sprintf("%s user biller", sb.Plan)
+}
+
+type orgBiller struct {
+	Plan string
+}
+
+func (ob orgBiller) Name() string {
+	return fmt.Sprintf("%s org biller", ob.Plan)
+}
+
+func (ob orgBiller) Charge(o org) bill {
+	amount := 2000.0
+	if ob.Plan == "pro" {
+		amount = 3000.0
+	}
+	return bill{
+		Customer: o,
+		Amount:   amount,
+	}
+}
+
+type customer interface {
+	GetBillingEmail() string
+}
+
+type bill struct {
+	Customer customer
+	Amount   float64
+}
+
+type user struct {
+	UserEmail string
+}
+
+func (u user) GetBillingEmail() string {
+	return u.UserEmail
+}
+
+type org struct {
+	Admin user
+	Name  string
+}
+
+func (o org) GetBillingEmail() string {
+	return o.Admin.GetBillingEmail()
+}
+
+//-------------------------------------------------------------------------------------------------
 
 func main() {
 	// firstInts, secondInts := splitAnySlice([]int{0, 1, 2, 3})
